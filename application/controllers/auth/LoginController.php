75c46772c911e3ab3d5427cccc321abbe1f8b168
dimas1818 <covdias@gmail.com>
2020-04-12 14:49:47 +0700

    update login. user in-active gak bisa masuk

--- application/controllers/auth/LoginController.php
+++ application/controllers/auth/LoginController.php
@@ -17,23 +17,64 @@ class LoginController extends CI_Controller {
 	}
 
 	function do_login() {
-		$this->form_validation->set_rules('username','username','required');
-		$this->form_validation->set_rules('password','password','required');
+		// $this->form_validation->set_rules('username','username','required');
+		// $this->form_validation->set_rules('password','password','required');
 
-		$this->form_validation->set_message('required', '<div class="alert alert-danger" id="error_alert_field" role="alert">
-					    <button type="button" class="close" data-dismiss="alert">x</button>
-					    <strong><i class="fas fa-exclamation-circle"></i> {field} </strong> Harus di Isi!
-					</div>');
+		// $this->form_validation->set_message('required', '<div class="alert alert-danger" id="error_alert_field" role="alert">
+		// 			    <button type="button" class="close" data-dismiss="alert">x</button>
+		// 			    <strong><i class="fas fa-exclamation-circle"></i> {field} </strong> Harus di Isi!
+		// 			</div>');
 
-		if ($this->form_validation->run() == TRUE) {
-			$username = $this->input->post('username');
-			$password = do_hash($this->input->post('password'));
+		// if ($this->form_validation->run() == TRUE) {
+		// 	$username = $this->input->post('username');
+		// 	$password = do_hash($this->input->post('password'));
 
-			$cek = $this->AuthModel->auth_cek($username,$password);
+		// 	$cek = $this->AuthModel->auth_cek($username,$password);
 
-			if ($cek->num_rows() > 0) {
-				$auth = $cek->row();
+		// 	if ($cek->num_rows() > 0) {
+		// 		$auth = $cek->row();
 
+		// 		if ($auth->active == 0) {
+		// 			$data['error'] = '<div class="alert alert-danger" id="error_alert" role="alert">
+		// 			    <button type="button" class="close" data-dismiss="alert">x</button>
+		// 			    <strong><i class="fas fa-exclamation-circle"></i> User not active!</strong>
+		// 			</div>';
+		//             $this->load->view('auth/login', $data);
+		// 		} else {
+		// 			$this->session->set_userdata('masuk',TRUE);
+		// 			$this->session->set_userdata('level',$auth->level);
+		// 		    $this->session->set_userdata('ses_id',$auth->id);
+		// 		    $this->session->set_userdata('ses_nama',$auth->name);
+		// 		    $this->session->set_userdata('ses_email',$auth->email);
+		// 		    $this->session->set_userdata('ses_username',$auth->email);
+		// 		    $this->session->set_userdata('ses_password',$auth->password);
+		// 		    $this->AuthModel->auth_up_login();
+		// 		    redirect('home');
+		// 		}
+		// 	} else {
+		// 		$data['error'] = '<div class="alert alert-danger" id="error_alert" role="alert">
+		// 			    <button type="button" class="close" data-dismiss="alert">x</button>
+		// 			    <strong><i class="fas fa-exclamation-circle"></i> Username / Password Salah!</strong>
+		// 			</div>';
+	 //            $this->load->view('auth/login', $data);
+		// 	}
+		// } else {
+		// 	$this->load->view('auth/Login');
+		// }
+
+		$username = $this->input->post('username');
+		$password = do_hash($this->input->post('password'));
+
+		$cek = $this->AuthModel->auth_cek($username,$password);
+
+		if ($cek->num_rows() > 0) {
+			$auth = $cek->row();
+
+			if ($auth->active == 0) {
+				$err = 0;
+				echo json_encode($err);
+				exit();
+			} else {
 				$this->session->set_userdata('masuk',TRUE);
 				$this->session->set_userdata('level',$auth->level);
 			    $this->session->set_userdata('ses_id',$auth->id);
@@ -42,16 +83,13 @@ class LoginController extends CI_Controller {
 			    $this->session->set_userdata('ses_username',$auth->email);
 			    $this->session->set_userdata('ses_password',$auth->password);
 			    $this->AuthModel->auth_up_login();
-			    redirect('home');
-			} else {
-				$data['error'] = '<div class="alert alert-danger" id="error_alert" role="alert">
-					    <button type="button" class="close" data-dismiss="alert">x</button>
-					    <strong><i class="fas fa-exclamation-circle"></i> Username / Password Salah!</strong>
-					</div>';
-	            $this->load->view('auth/login', $data);
+			    echo json_encode(base_url('home'));
+			    exit();
 			}
 		} else {
-			$this->load->view('auth/Login');
+			$err = 0;
+			echo json_encode($err);
+			exit();
 		}
 	}
 }
